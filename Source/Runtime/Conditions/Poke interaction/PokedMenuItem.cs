1dde33ebe6a3f14628ffdf62d3071e24cadc5e1a
David Lähner <david.laehner@gmail.com>
2023-03-13 17:47:07 +0100

    Added Poke interaction
    The poke interaction triggers when an object is poked.
    The provided implementation uses the poke functionality of XR interaction toolkit.

---
+++ Source/Runtime/Conditions/Poke interaction/PokedMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using VRBuilder.BasicInteraction.Conditions;
+using VRBuilder.Core.Conditions;
+using VRBuilder.Editor.UI.StepInspector.Menu;
+
+namespace VRBuilder.Editor.BasicInteraction.UI.Conditions
+{
+    public class PokedMenuItem : MenuItem<ICondition>
+    {
+        public override string DisplayedName { get; } = "Interaction/Poke Object";
+
+        public override ICondition GetNewItem()
+        {
+            return new PokedCondition();
+        }
+    }
+}
